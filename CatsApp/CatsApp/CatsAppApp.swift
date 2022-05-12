c30c70608417206e5ce0b620f407557ea22d7241
OBaller <naseemoyebola@gmail.com>
2022-05-12 11:58:01 +0100

    initial comit

---
+++ CatsApp/CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by Naseem Oyebola on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
