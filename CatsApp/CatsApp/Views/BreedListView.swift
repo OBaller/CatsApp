1931d798ab39311c3693f986a7b5c724701908a7
OBaller <naseemoyebola@gmail.com>
2022-05-12 16:08:32 +0100

    Display Views

--- CatsApp/CatsApp/Views/BreedListView.swift
+++ CatsApp/CatsApp/Views/BreedListView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct BreedListView: View {
     let breeds: [Breed]
+    @State private var searchText: String = ""
+    var filteredBreeds: [Breed] {
+        if searchText.count == 0 {
+            return breeds
+        } else {
+            return breeds.filter {
+                $0.name.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(filteredBreeds) { breed in
+                    NavigationLink {
+                        BreedDetailView(breed: breed)
+                    } label: {
+                        BreedRow(breed: breed)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("Find Your Perfect Cat")
+            .searchable(text: $searchText)
+        }
     }
 }
 
 struct BreedListView_Previews: PreviewProvider {
     static var previews: some View {
-        BreedListView(breeds: [Breed]())
+        BreedListView(breeds: BreedFetcher.successState().breeds)
     }
 }
