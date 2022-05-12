a8358a28f4992e7d3f20ed5da6f899cc06b5a3ea
OBaller <naseemoyebola@gmail.com>
2022-05-12 12:19:35 +0100

    Add model

---
+++ CatsApp/CatsApp/Model/BreedImage.swift
@@ -0,0 +1,16 @@
+//
+//  BreedImage.swift
+//  CatsApp
+//
+//  Created by Naseem Oyebola on 12/05/2022.
+//
+
+import Foundation
+
+struct BreedImage: Codable {
+    let height: Int?
+    let id: String?
+    let url: String?
+    let width: Int?
+    
+}
